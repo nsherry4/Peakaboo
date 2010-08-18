a6b441221fd8a064cda893fc5025c62d898cf27d
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2010-08-17 21:21:57 -0400

    added missing files

---
+++ Peakaboo_Filters/src/peakaboo/filter/filters/advanced/Identity.java
@@ -0,0 +1,73 @@
+package peakaboo.filter.filters.advanced;
+
+import peakaboo.filter.AbstractFilter;
+import scidraw.drawing.plot.painters.PlotPainter;
+import scitypes.Spectrum;
+
+
+public class Identity extends AbstractFilter
+{
+
+	@Override
+	public boolean canFilterSubset()
+	{
+		return true;
+	}
+
+
+	@Override
+	protected Spectrum filterApplyTo(Spectrum data, boolean cache)
+	{
+		return data;
+	}
+
+
+	@Override
+	public String getFilterDescription()
+	{
+		return "This filter is the identity function -- it does no processing to the data";
+	}
+
+
+	@Override
+	public String getFilterName()
+	{
+		return "None";
+	}
+
+
+	@Override
+	public FilterType getFilterType()
+	{
+		return FilterType.ADVANCED;
+	}
+
+
+	@Override
+	public PlotPainter getPainter()
+	{
+		return null;
+	}
+
+
+	@Override
+	public void initialize()
+	{
+
+	}
+
+
+	@Override
+	public boolean showFilter()
+	{
+		return false;
+	}
+
+
+	@Override
+	public boolean validateParameters()
+	{
+		return true;
+	}
+
+}
