b36e16cac25fd94a8e17a66dc2815a314521060a
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2010-08-17 21:24:03 -0400

    added Corrections files

---
+++ Peakaboo_Mapping/src/peakaboo/mapping/correction/Corrections.java
@@ -0,0 +1,75 @@
+package peakaboo.mapping.correction;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import commonenvironment.AbstractFile;
+import fava.Functions;
+import fava.datatypes.Pair;
+import fava.lists.FList;
+import fava.signatures.FnMap;
+
+
+import peakaboo.common.DataTypeFactory;
+import peakaboo.curvefit.peaktable.Element;
+import peakaboo.curvefit.peaktable.TransitionSeries;
+import peakaboo.curvefit.peaktable.TransitionSeriesMode;
+import peakaboo.curvefit.peaktable.TransitionSeriesType;
+
+
+public class Corrections
+{
+
+	private String name;
+	private FList<Pair<TransitionSeries, Float>> correctionPairs;
+	
+	public Corrections(AbstractFile file) throws IOException
+	{
+	
+		correctionPairs = new FList<Pair<TransitionSeries, Float>>();
+		
+		BufferedReader reader = file.getReader();
+		name = reader.readLine();
+		
+		while (true)
+		{
+			String line = reader.readLine();
+			if (line == null) break;
+			
+			String parts[] = line.split(",");
+			
+			Element e = Element.values()[Integer.parseInt(parts[1])-1];
+			TransitionSeriesType tst = TransitionSeriesType.valueOf(parts[0]);
+			
+			float factor = Float.parseFloat(parts[2]);
+			
+			//add a dummy transitionseries -- this will pass the TransitionSeries#equals test with the real thing
+			correctionPairs.add(new Pair<TransitionSeries, Float>(new TransitionSeries(e, tst, TransitionSeriesMode.PRIMARY), factor));
+		}
+		
+		
+	}
+	
+	public Float getCorrection(final TransitionSeries ts)
+	{		
+		FList<Pair<TransitionSeries, Float>> matches = correctionPairs.filter(new FnMap<Pair<TransitionSeries, Float>, Boolean>() {
+			
+			public Boolean f(Pair<TransitionSeries, Float> p)
+			{
+				return p.first.equals(ts);
+			}
+		});
+		
+		if (matches .size() == 0) return null;
+		return matches.head().second;
+		
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+}
---
+++ Peakaboo_Mapping/src/peakaboo/mapping/correction/CorrectionsManager.java
@@ -0,0 +1,46 @@
+package peakaboo.mapping.correction;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+
+import commonenvironment.AbstractFile;
+
+import peakaboo.common.DataTypeFactory;
+import peakaboo.curvefit.peaktable.PeakTableReader;
+
+
+public class CorrectionsManager
+{
+	
+	private static Map<String, Corrections> correctionSets;
+	private static boolean initialized = false;
+	
+	private static void initialize()
+	{
+		initialized = true;
+		correctionSets = DataTypeFactory.<String, Corrections>map();
+		try
+		{
+			correctionSets.put("WL", new Corrections(new AbstractFile(PeakTableReader.class.getResource("/peakaboo/mapping/correction/wl.csv")))); 
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Collection<String> getCorrectionList()
+	{
+		if (!initialized) initialize();
+		return correctionSets.keySet();
+	}
+	
+	public static Corrections getCorrections(String name)
+	{
+		if (!initialized) initialize();
+		return correctionSets.get(name);
+	}
+	
+}
