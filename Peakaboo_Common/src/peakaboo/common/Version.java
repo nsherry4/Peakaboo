f37f56afa4dce1fa30644871fc09389d288bbc58
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2011-04-12 11:20:27 -0400

    Updated version information, set release to false

--- Peakaboo_Common/src/peakaboo/common/Version.java
+++ Peakaboo_Common/src/peakaboo/common/Version.java
@@ -2,17 +2,17 @@ package peakaboo.common;
 
 public class Version {
 
-	public final static boolean release = true;
+	public final static boolean release = false;
 	public final static boolean rc = false;
 	
 	
-	public final static int versionNo = 3;
-	public final static String buildDate = "2010-08-23";
-	public final static String longVersionNo = ( (release) ? "3.0.0" : rc ? "3.0.0 RC6" : "2.99.21");
+	public final static int versionNo = 4;
+	public final static String buildDate = "2011-04-11";
+	public final static String longVersionNo = ( (release) ? "4.0.0" : rc ? "4.0.0 RC1" : "3.97.0");
 	public final static String logo = (release) ? "logo" : rc ? "rclogo" : "devlogo";
 	public final static String icon = (release) ? "icon" : rc ? "rcicon" : "devicon";
 	public final static String program_name = "Peakaboo";
-	public final static String title = program_name + " " + versionNo + ((release) ? "" : rc ? " [Release Candidate]" : " [Development Release]");
+	public final static String title = program_name + " " + (release ? versionNo : longVersionNo) + ((release) ? "" : rc ? " [Release Candidate]" : " [Development Release]");
 	
 	
 }
