a4108eafad393ade0be637e439e14983e88fadda
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2012-02-03 15:57:46 -0500

    Added missing java files for handling DataSources at the UI level

---
+++ Peakaboo_SwingGUI/src/peakaboo/ui/swing/plotting/datasource/DataSourceLookup.java
@@ -0,0 +1,73 @@
+package peakaboo.ui.swing.plotting.datasource;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import peakaboo.datasource.plugin.AbstractDSP;
+
+import commonenvironment.AbstractFile;
+import commonenvironment.IOOperations;
+import fava.functionable.FList;
+import fava.signatures.FnMap;
+
+public class DataSourceLookup
+{
+
+	public static List<AbstractDSP> findDataSourcesForFiles(List<String> filenames, List<AbstractDSP> dsps)
+	{	
+		
+		List<AbstractDSP> datasources = new ArrayList<AbstractDSP>();
+		
+		if (filenames.size() == 1)
+		{
+			String filename = filenames.get(0);
+			
+			for (AbstractDSP datasource : dsps)
+			{
+			
+				if ( !matchFileExtension(filename, datasource.getFileExtensions()) ) continue;
+				if ( !datasource.canRead(filename) ) continue;
+				datasources.add(datasource);
+
+				
+			}//for datasources
+		}
+		else
+		{
+		
+			//loop over every datasource
+			for (AbstractDSP datasource : dsps)
+			{
+				
+				if ( !matchFileExtensions(filenames, datasource.getFileExtensions()) ) continue;
+				if ( !datasource.canRead(filenames) ) continue;
+				datasources.add(datasource);
+				
+			}
+			
+		}
+		
+		return datasources;
+		
+	}
+	
+	private static boolean matchFileExtension(String filename, Collection<String> dsexts)
+	{
+		for (String dsext : dsexts)
+		{
+			
+			if (IOOperations.getFileExt(filename).compareToIgnoreCase(dsext) == 0) return true;
+		}
+		return false;
+	}
+	
+	private static boolean matchFileExtensions(Collection<String> filenames, Collection<String> dsexts)
+	{
+		for (String filename : filenames)
+		{
+			if (!matchFileExtension(filename, dsexts)) return false;
+		}
+		return true;
+	}
+}
---
+++ Peakaboo_SwingGUI/src/peakaboo/ui/swing/plotting/datasource/DataSourceSelection.java
@@ -0,0 +1,135 @@
+package peakaboo.ui.swing.plotting.datasource;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import fava.functionable.FList;
+
+import peakaboo.datasource.plugin.AbstractDSP;
+import peakaboo.datasource.plugin.plugins.CDFMLSaxDSP;
+import peakaboo.datasource.plugin.plugins.ScienceStudioDSP;
+import swidget.Swidget;
+import swidget.icons.StockIcon;
+import swidget.widgets.ButtonBox;
+import swidget.widgets.ImageButton;
+import swidget.widgets.Spacing;
+import swidget.widgets.gradientpanel.TitleGradientPanel;
+import swidget.widgets.toggle.ComplexToggle;
+import swidget.widgets.toggle.ComplexToggleGroup;
+
+
+public class DataSourceSelection extends JDialog
+{
+	
+	private Map<ComplexToggle, AbstractDSP> toggleMap;
+	private AbstractDSP selected;
+	
+	public DataSourceSelection()
+	{
+		
+	}
+	
+	public AbstractDSP pickDSP(Window parent, List<AbstractDSP> dsps)
+	{	
+		
+		toggleMap = new HashMap<ComplexToggle, AbstractDSP>();
+		
+		setTitle("Please Select Data Format");
+		Container c = getContentPane();
+		
+		c.setLayout(new BorderLayout());
+		setResizable(false);
+		setModal(true);
+		
+		TitleGradientPanel title = new TitleGradientPanel("Peakaboo can't decide what format this data is in.", true);
+		title.setBorder(Spacing.bMedium());
+		//JLabel title = new JLabel("<html><b>Peakaboo can't decide what format this data is</b></html>", JLabel.CENTER);
+		//title.setFont(title.getFont().deriveFont(title.getFont().getSize() * 1.5f));
+		//title.setBorder(Spacing.bMedium());
+		c.add(title, BorderLayout.NORTH);
+	
+		
+		JPanel optionPanel = new JPanel();
+		optionPanel.setBorder(Spacing.bHuge());
+		optionPanel.setLayout(new BoxLayout(optionPanel, BoxLayout.Y_AXIS));
+		
+		final List<ComplexToggle> toggleButtons = new ArrayList<ComplexToggle>();
+		ComplexToggle toggle;
+		final ComplexToggleGroup group = new ComplexToggleGroup();
+		for (AbstractDSP dsp : dsps)
+		{
+			toggle = new ComplexToggle("", dsp.getDataFormat(), dsp.getDataFormatDescription());
+			toggleMap.put(toggle, dsp);
+			group.registerButton(toggle);	
+			toggleButtons.add(toggle);
+			
+			optionPanel.add(toggle);
+			optionPanel.add(Box.createVerticalStrut(Spacing.medium));
+		}
+		toggleButtons.get(0).setSelected(true);
+		
+		add(optionPanel, BorderLayout.CENTER);
+		
+		
+		
+		ButtonBox box = new ButtonBox();
+		ImageButton ok = new ImageButton(StockIcon.CHOOSE_OK, "OK");
+		ImageButton cancel = new ImageButton(StockIcon.CHOOSE_CANCEL, "Cancel");
+		
+		box.addRight(cancel);
+		box.addRight(ok);
+		add(box, BorderLayout.SOUTH);
+		
+		ok.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+				selected = toggleMap.get(toggleButtons.get(group.getToggledIndex()));
+				setVisible(false);
+			}
+		});
+		
+		cancel.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+				selected = null;
+				setVisible(false);
+			}
+		});
+		
+		
+		pack();
+		setLocationRelativeTo(parent);
+		setVisible(true);
+		
+		return selected;		
+		
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		Swidget.initialize();
+		DataSourceSelection dss = new DataSourceSelection();
+		AbstractDSP dsp = dss.pickDSP(null, new FList<AbstractDSP>(new ScienceStudioDSP(), new CDFMLSaxDSP()));
+		System.out.println(dsp.getDataFormat());
+	}
+	
+}
