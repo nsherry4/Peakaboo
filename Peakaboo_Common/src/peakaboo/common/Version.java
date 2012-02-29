7d5daaec3d6f664db3e2841bc7997ec46c0e35f4
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2012-02-29 17:01:40 -0500

    Peakaboo 4 RC version change

--- Peakaboo_Common/src/peakaboo/common/Version.java
+++ Peakaboo_Common/src/peakaboo/common/Version.java
@@ -3,12 +3,12 @@ package peakaboo.common;
 public class Version {
 
 	public final static boolean release = false;
-	public final static boolean rc = false;
+	public final static boolean rc = true;
 	
 	
 	public final static int versionNo = 4;
-	public final static String buildDate = "2012-01-12";
-	public final static String longVersionNo = ( (release) ? "4.0.0" : rc ? "4.0.0 RC1" : "3.98.0");
+	public final static String buildDate = "2012-02-29";
+	public final static String longVersionNo = ( (release) ? "4.0.0" : rc ? "4.0.0 RC1" : "3.98.1");
 	public final static String logo = (release) ? "logo" : rc ? "rclogo" : "devlogo";
 	public final static String icon = (release) ? "icon" : rc ? "rcicon" : "devicon";
 	public final static String program_name = "Peakaboo";
