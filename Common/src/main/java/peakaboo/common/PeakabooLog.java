805c03d1746ce246146d5ab66d37d42220696ed5
Nathaniel Sherry <nsherry4@uwo.ca>
2018-03-07 17:04:15 -0500

    PeakabooLog created the log file before handing it off to the logger, working around bug JDK-8189953. Log files also have a timestamp in the name so that they can be retrieved more easily for analysis

--- Common/src/main/java/peakaboo/common/PeakabooLog.java
+++ Common/src/main/java/peakaboo/common/PeakabooLog.java
@@ -2,6 +2,7 @@ package peakaboo.common;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,7 +14,16 @@ public class PeakabooLog {
 		try {
 			File appDir = Configuration.appDir();
 			appDir.mkdirs();
-			FileHandler handler = new FileHandler(appDir.getPath() + "/Peakaboo.log");
+			String filename = appDir.getPath() + "/Peakaboo-" + new Timestamp(System.currentTimeMillis()) + ".log";
+			
+			
+			//Workaround for JDK-8189953
+			File file = new File(filename);
+			file.createNewFile();
+			////////////////////////////
+			
+			
+			FileHandler handler = new FileHandler(filename);
 			handler.setFormatter(new SimpleFormatter());
 			Logger.getLogger("").addHandler(handler);
 		} catch (SecurityException | IOException e) {
