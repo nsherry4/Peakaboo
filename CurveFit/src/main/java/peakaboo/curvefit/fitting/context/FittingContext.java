51475678dbcb3e83d47c62a6796f8da29f1052c4
Nathaniel Sherry <nsherry4@uwo.ca>
2018-04-11 16:01:17 -0400

    Move Escape Peak intensity calculations to EscapePeakType

--- CurveFit/src/main/java/peakaboo/curvefit/fitting/context/FittingContext.java
+++ CurveFit/src/main/java/peakaboo/curvefit/fitting/context/FittingContext.java
@@ -2,6 +2,7 @@ package peakaboo.curvefit.fitting.context;
 
 import peakaboo.curvefit.fitting.FittingParameters;
 import peakaboo.curvefit.peaktable.Element;
+import peakaboo.curvefit.transition.EscapePeakType;
 import peakaboo.curvefit.transition.Transition;
 import peakaboo.curvefit.transition.TransitionSeriesType;
 
@@ -24,7 +25,7 @@ public class FittingContext {
 	//escape peak
 	public FittingContext(FittingParameters parameters, Transition transition, Transition escape, Element element, TransitionSeriesType type) {
 		this.energy = transition.energyValue - escape.energyValue;
-		this.height = transition.relativeIntensity * escape.relativeIntensity * escapeIntensity(element);
+		this.height = transition.relativeIntensity * escape.relativeIntensity * EscapePeakType.escapeIntensity(element);
 		this.transition = transition;
 		this.type = type;
 		this.parameters = parameters;
@@ -46,23 +47,5 @@ public class FittingContext {
 	public float getFWHM() {
 		return parameters.getFWHM(transition);
 	}
-
-
-	private float escapeIntensity(Element e)
-	{
-		/*
-		 * The paper
-		 * 
-		 * " Measurement and calculation of escape peak intensities in synchrotron radiation X-ray fluorescence analysis
-		 * S.X. Kang a, X. Sun a, X. Ju b, Y.Y. Huang b, K. Yao a, Z.Q. Wu a, D.C. Xian b"
-		 * 
-		 * provides a listing of escape peak intensities relative to the real peak by element. By taking this data into
-		 * openoffice and fitting an exponential regression line to it, we arrive at the formula esc(z) = (543268.59
-		 * z^-4.48)%
-		 */
-
-		return 543268.59f * (float) Math.pow((e.ordinal() + 1), -4.48) / 100.0f;
-	}
-
 	
 }
--- CurveFit/src/main/java/peakaboo/curvefit/transition/EscapePeakType.java
+++ CurveFit/src/main/java/peakaboo/curvefit/transition/EscapePeakType.java
@@ -66,4 +66,21 @@ public enum EscapePeakType
 	{
 		return SILICON;
 	}
+	
+
+	public static float escapeIntensity(Element e)
+	{
+		/*
+		 * The paper
+		 * 
+		 * " Measurement and calculation of escape peak intensities in synchrotron radiation X-ray fluorescence analysis
+		 * S.X. Kang a, X. Sun a, X. Ju b, Y.Y. Huang b, K. Yao a, Z.Q. Wu a, D.C. Xian b"
+		 * 
+		 * provides a listing of escape peak intensities relative to the real peak by element. By taking this data into
+		 * openoffice and fitting an exponential regression line to it, we arrive at the formula esc(z) = (543268.59
+		 * z^-4.48)%
+		 */
+
+		return 543268.59f * (float) Math.pow((e.ordinal() + 1), -4.48) / 100.0f;
+	}
 }
