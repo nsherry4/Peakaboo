021886c321423bd9950499e586a126d85d0bdc28
Nathaniel Sherry <nsherry4@uwo.ca>
2018-06-29 16:41:53 -0400

    LeastSquaresFittingSolver only examining important channels, validating to make sure curve scale >= 0

--- CurveFit/src/main/java/peakaboo/curvefit/curve/fitting/solver/LeastSquaresFittingSolver.java
+++ CurveFit/src/main/java/peakaboo/curvefit/curve/fitting/solver/LeastSquaresFittingSolver.java
@@ -2,7 +2,10 @@ package peakaboo.curvefit.curve.fitting.solver;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.math3.fitting.leastsquares.LeastSquaresBuilder;
@@ -10,6 +13,7 @@ import org.apache.commons.math3.fitting.leastsquares.LeastSquaresOptimizer;
 import org.apache.commons.math3.fitting.leastsquares.LeastSquaresProblem;
 import org.apache.commons.math3.fitting.leastsquares.LevenbergMarquardtOptimizer;
 import org.apache.commons.math3.fitting.leastsquares.MultivariateJacobianFunction;
+import org.apache.commons.math3.fitting.leastsquares.ParameterValidator;
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealMatrix;
@@ -47,18 +51,37 @@ public class LeastSquaresFittingSolver implements FittingSolver {
 				);
 		}
 		
-		
+		Set<Integer> intenseChannels = new LinkedHashSet<>();
+		for (Curve curve : fittings.getCurves()) {
+			for (int channel : curve.getIntenseRanges()) {
+				intenseChannels.add(channel);
+			}
+		}
 		
 		
 		//number of variables (1 scaling value per curve
 		
 		int curveCount = visibleCurves.size();
-		int channelCount = data.size();
+		int channelCount = intenseChannels.size();
 		
 		double[] guesses = new double[curveCount];
 		for (int curveIndex = 0; curveIndex < visibleCurves.size(); curveIndex++) {
 			guesses[curveIndex] = fitter.fit(data, visibleCurves.get(curveIndex)).getCurveScale();
 		}
+		
+		
+		//Validate that no scale value falls below 0 just to make things work out okay.
+		ParameterValidator validator = point -> {
+			for (int i = 0; i < point.getDimension(); i++) {
+				if (point.getEntry(i) < 0) {
+					point.setEntry(i, 0);
+				}
+			}
+			return point;
+		};
+		
+		
+		
 		
 		MultivariateJacobianFunction distanceFromData = point -> {
 			
@@ -73,31 +96,37 @@ public class LeastSquaresFittingSolver implements FittingSolver {
 			RealMatrix matrix = new Array2DRowRealMatrix(channelCount, curveCount);
 			
 			
-			for (int channel = 0; channel < channelCount; channel++) {
+			int channelIndex = 0;
+			for (int channel : intenseChannels) {
 				float distance = residual.get(channel);
 				float absdist = Math.abs(distance);
+				
+				
+				
 				if (distance < 0) {
 					absdist *= 1000;
 				}
-				
-				
-				vector.setEntry(channel, absdist);
+				vector.setEntry(channelIndex, absdist);
 				
 				float deriv = 0;
 				if (distance != 0) {
 					deriv = distance / absdist;
 				}
 				
+				System.out.println(channel + ":  " + distance + ", " + absdist + ", " + deriv);
+				
 				int curveIndex = 0;
 				for (Curve curve : visibleCurves) {
 					if (curve.getIntenseRanges().contains(channel)) {
-						matrix.setEntry(channel, curveIndex, -deriv);
+						matrix.setEntry(channelIndex, curveIndex, -deriv);
 					}
 					curveIndex++;
 				}
 				
-
+				channelIndex++;
 			}
+			
+			System.out.println("---------------------");
 			
 			// TODO Auto-generated method stub
 			return new Pair<>(vector, matrix);
@@ -111,6 +140,7 @@ public class LeastSquaresFittingSolver implements FittingSolver {
 			.lazyEvaluation(false)
 			.maxEvaluations(10000)
 			.maxIterations(10000)
+			.parameterValidator(validator)
 			.build();
 		
 		LeastSquaresOptimizer.Optimum optimum = new LevenbergMarquardtOptimizer().optimize(problem);
