19465590e442d57b003a53944ca966babebcc9aa
Nathaniel Sherry <nsherry4@uwo.ca>
2018-07-12 15:28:48 -0400

    Switch windows packaging to launch4j standalone exe, added support for rpm

--- Packaging/build.rb
+++ Packaging/build.rb
@@ -38,23 +38,26 @@ def setup
 	dapppath = "./deb/5.0/usr/share/Peakaboo/"
 	dbinpath = "./deb/5.0/usr/bin/"
 	dconpath = "./deb/5.0/DEBIAN/"
-	
-	resources = [["shared/icon.png", dapppath], ["shared/logo.png", dapppath], ["linux/peakaboo", dbinpath], ["linux/control", dconpath]]
-
-	doSetup(jarfile, dapppath, resources)
+	resources = [["shared/icon.png", dapppath], ["shared/logo.png", dapppath], ["linux/peakaboo", dbinpath], ["linux/control", dconpath], ["linux/Peakaboo.desktop", "./deb/5.0/usr/share/applications/"]]
+	doSetup(jarfile, dapppath, resources, false)
 
 
-
+	#RPM Package
+	apppath = "./rpm/5.0/usr/share/Peakaboo"
+	binpath = "./rpm/5.0/usr/bin/"
+	resources = [["shared/icon.png", apppath], ["shared/logo.png", apppath], ["linux/peakaboo", binpath], ["linux/Peakaboo.desktop", "./rpm/5.0/usr/share/applications/"]]
+	doSetup(jarfile, apppath, resources, false)
 
 	#Win32 Package
-	winpath = "./windows/Peakaboo/"
+	#winpath = "./windows/Peakaboo/"
+	#resources = [["windows/Logo.ico", winpath], ["windows/peakaboo.vbs", winpath], ["windows/Peakaboo.exe", winpath]]
+	##doSetup(jarfile, winpath, resources)
+	#doSetup(jarfile, winpath, resources, false)
 	
-	resources = [["windows/Logo.ico", winpath], ["windows/peakaboo.vbs", winpath], ["windows/Peakaboo.exe", winpath]]
 	
-	#doSetup(jarfile, winpath, resources)
+	winpath= "./windows-launch4j/"
+	resources = [["windows/Logo.ico", winpath]]
 	doSetup(jarfile, winpath, resources, false)
-	
-	
 	
 	
 	#Mac OS Package
@@ -81,10 +84,18 @@ puts "Building Debian Package..."
 `cp -f ./deb/Peakaboo.deb ./build/`
 puts "DONE\n\n"
 
+puts "Building Red Hat Package..."
+`cd ./rpm && ./generate.sh`
+`cp -f ./rpm/Peakaboo*.rpm ./build/`
+puts "DONE\n\n"
+
 puts "Building Windows Package..."
-`rm -rf ./windows/_win32/_win32/*.exe`
-`cd ./windows/_win32/ && ./buildWindowsInstaller.sh`
-`cp ./windows/_win32/_win32/*.exe ./build/`
+#`rm -rf ./windows/_win32/_win32/*.exe`
+#`cd ./windows/_win32/ && ./buildWindowsInstaller.sh`
+#`cp ./windows/_win32/_win32/*.exe ./build/`
+`rm -rf ./windows-launch4j/*.exe`
+`cd ./windows-launch4j/ && ./build.sh`
+`cp ./windows-launch4j/*.exe ./build/`
 puts "DONE\n\n"
 
 puts "Building Mac Package..."
---
+++ Packaging/windows-launch4j/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar ./launch4j/launch4j.jar peakaboo.xml
