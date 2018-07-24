3a8fb7503fa27a7092c689fb8149c77a99ff2489
Nathaniel Sherry <nsherry4@uwo.ca>
2018-07-24 11:07:17 -0400

    Removed old nullsoft installer for windows

--- Packaging/windows/_win32/buildWindowsInstaller.sh
+++
@@ -1,2 +0,0 @@
-export WINEPREFIX=/home/nathaniel/.wine-peakaboo/
-wine '/home/nathaniel/.wine-peakaboo/drive_c/Program Files/NSIS/makensis.exe' Z:\\home\\nathaniel\\Projects\\SS\\Git\\Peakaboo\\Packaging\\windows\\_win32\\_win32\\installer.nsi
