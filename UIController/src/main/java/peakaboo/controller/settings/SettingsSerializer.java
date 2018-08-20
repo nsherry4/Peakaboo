9ba6f8a756862db632230861e0bae4953845b74a
Nathaniel Sherry <nsherry4@uwo.ca>
2018-08-20 15:13:56 -0400

    Loading session files is more forgiving of unknown fields, allowing better backwards compat going forward

--- UIController/src/main/java/peakaboo/controller/settings/SettingsSerializer.java
+++ UIController/src/main/java/peakaboo/controller/settings/SettingsSerializer.java
@@ -3,6 +3,7 @@ package peakaboo.controller.settings;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.DumperOptions.FlowStyle;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.representer.Representer;
 
 public class SettingsSerializer {
 
@@ -10,7 +11,13 @@ public class SettingsSerializer {
 	 * Decodes a serialized data object from yaml
 	 */
 	public static <T> T deserialize(String yaml) {
-		Yaml y = new Yaml();
+				
+		//be forgiving of fields which are unfamiliar, this may 
+		//be a peakaboo session file from the future
+		Representer representer = new Representer();
+		representer.getPropertyUtils().setSkipMissingProperties(true);
+		Yaml y = new Yaml(representer);
+		
 		T data = (T)y.load(yaml);
 		return data;
 	}
