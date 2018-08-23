b93d7fc72ad96207c7db1b1ee7e2b1f9a80a0132
Nathaniel Sherry <nsherry4@uwo.ca>
2018-08-23 10:52:07 -0400

    SimplePhysicalSize stores coords on disk if the MemoryProfile is small

--- DataSource/src/main/java/peakaboo/datasource/model/components/physicalsize/SimplePhysicalSize.java
+++ DataSource/src/main/java/peakaboo/datasource/model/components/physicalsize/SimplePhysicalSize.java
@@ -1,26 +1,49 @@
 package peakaboo.datasource.model.components.physicalsize;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import net.sciencestudio.scratch.ScratchEncoder;
+import net.sciencestudio.scratch.encoders.CompoundEncoder;
+import net.sciencestudio.scratch.encoders.compressors.Compressors;
+import net.sciencestudio.scratch.encoders.serializers.Serializers;
+import net.sciencestudio.scratch.list.ScratchLists;
+import peakaboo.common.MemoryProfile;
+import peakaboo.common.MemoryProfile.Size;
 import scitypes.Bounds;
 import scitypes.Coord;
+import scitypes.ISpectrum;
 import scitypes.SISize;
+import scitypes.SparsedList;
 
 public class SimplePhysicalSize implements PhysicalSize {
 
-	private Map<Integer, Coord<Number>> points = new HashMap<>();
+	private List<Coord<Number>> points;
 	private Coord<Bounds<Number>> dimensions = null;
 	
 	private SISize units;
 	
+	@SuppressWarnings("unchecked")
 	public SimplePhysicalSize(SISize units) {
 		this.units = units;
+		
+		List<Coord<Number>> scratch = new ArrayList<>();
+		if (MemoryProfile.size == Size.SMALL) {
+			//Physical coordinates on large maps can eat up ~10MB, which is enough to 
+			//warrant disk-based storage when we're low on memory 
+			
+			ScratchEncoder<?> fst = Serializers.fst(Coord.class);
+			ScratchEncoder<byte[]> lz4 = Compressors.lz4fast();
+			scratch = (List<Coord<Number>>) ScratchLists.tryDiskBacked(new CompoundEncoder<>(fst, lz4));
+		}
+		points = new SparsedList<>(scratch);
 	}
 	
 	@Override
 	public Coord<Number> getPhysicalCoordinatesAtIndex(int index) throws IndexOutOfBoundsException {
-		if (points.containsKey(index)) {
+		if (points.get(index) != null) {
 			return points.get(index);
 		} else {
 			throw new IndexOutOfBoundsException("Index " + index + " has no data");
@@ -35,7 +58,8 @@ public class SimplePhysicalSize implements PhysicalSize {
 			float maxx = -Float.MAX_VALUE;
 			float maxy = -Float.MAX_VALUE;
 			
-			for (Coord<Number> point : points.values()) {
+			for (Coord<Number> point : points) {
+				if (point == null) { continue; }
 				if (point.x.floatValue() < minx) minx = point.x.floatValue();
 				if (point.y.floatValue() < miny) miny = point.y.floatValue();
 				if (point.x.floatValue() > maxx) maxx = point.x.floatValue();
@@ -55,7 +79,7 @@ public class SimplePhysicalSize implements PhysicalSize {
 	}
 	
 	public void putPoint(int index, Coord<Number> point) {
-		points.put(index, point);
+		points.set(index, point);
 	}
-
+	
 }
