3a1dc38e8307f6db98f840679399e47620a0a479
Nathaniel Sherry <nsherry4@uwo.ca>
2018-08-31 12:01:21 -0400

    Split out a new libpeakaboo repository, leaving the UI components in this repository

--- DataSource/src/main/java/peakaboo/datasink/plugin/DataSinkPlugin.java
+++
@@ -1,8 +0,0 @@
-package peakaboo.datasink.plugin;
-
-import net.sciencestudio.bolt.plugin.core.BoltPlugin;
-import peakaboo.datasink.model.DataSink;
-
-public interface DataSinkPlugin extends DataSink, BoltPlugin {
-
-}
--- DataSource/src/main/java/peakaboo/datasource/plugin/DataSourcePlugin.java
+++
@@ -1,8 +0,0 @@
-package peakaboo.datasource.plugin;
-
-import net.sciencestudio.bolt.plugin.core.BoltPlugin;
-import peakaboo.datasource.model.DataSource;
-
-public interface DataSourcePlugin extends DataSource, BoltPlugin {
-
-}
