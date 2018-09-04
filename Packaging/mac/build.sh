0cd511e11b3f9715b9f122cff1ba6513ae067330
Nathaniel Sherry <nsherry4@uwo.ca>
2018-09-04 12:23:56 -0400

    Mac packages not bundling Peakaboo.app properly, not supporting hidpi, addresses issue #81

--- Packaging/mac/build.sh
+++ Packaging/mac/build.sh
@@ -2,7 +2,10 @@
 
 cd build
 
-../jar2app/jar2app.py ./Peakaboo.jar -n "Peakaboo $1" -d "Peakaboo $2" -i ./peakaboo.icns -b peakaboo -v "$1" -s $2 -m peakaboo.ui.swing.Peakaboo -j "-Xmx1024M" -e Peakaboo --low-res-mode
+../jar2app/jar2app.py ./Peakaboo.jar -n "Peakaboo $1" -d "Peakaboo $2" -i ./peakaboo.icns -b peakaboo -v "$1" -s $2 -m peakaboo.ui.swing.Peakaboo -j "-Xmx1024M" -e Peakaboo
 mv "Peakaboo $1.app/" "Peakaboo.app"
+mkdir App
+mv Peakaboo.app App
 
-sudo ../dir2dmg.sh  ./Peakaboo.app/ "Peakaboo $1.dmg" "Peakaboo $2"
+
+sudo ../dir2dmg.sh  ./App/ "Peakaboo $1.dmg" "Peakaboo $2"
