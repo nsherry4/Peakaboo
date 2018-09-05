ddfc40731a5e4c4bd50699f32f3c747516f9378c
Nathaniel Sherry <nsherry4@uwo.ca>
2018-09-05 14:57:01 -0400

    Removed lingering CurveFit folder which was consolidated into libpeakaboo

--- LibPeakaboo/src/main/java/peakaboo/datasource/model/DataSource.java
+++ LibPeakaboo/src/main/java/peakaboo/datasource/model/DataSource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import net.sciencestudio.autodialog.model.Group;
+import net.sciencestudio.autodialog.model.Value;
 import peakaboo.datasource.model.components.datasize.DataSize;
 import peakaboo.datasource.model.components.fileformat.FileFormat;
 import peakaboo.datasource.model.components.interaction.Interaction;
@@ -20,7 +21,15 @@ public interface DataSource
 		return true;
 	}
 
-	
+	/**
+	 * Called before {@link DataSource#read(List)} with the same {@link List} of
+	 * {@link Path}s. This gives the DataSource a chance to request more information
+	 * about how the selected files shoudl be read.
+	 * 
+	 * @param paths the list of files which will be read
+	 * @return the {@link Value} {@link Group} to be responded to, or
+	 *         {@link Optional#empty()} if there are no parameters.
+	 */
 	Optional<Group> getParameters(List<Path> paths);
 	Optional<Metadata> getMetadata();
 	Optional<DataSize> getDataSize();
