63c319dfb478d0a781a7d9fc8d95e6638a51fab9
Nathaniel Sherry <nsherry4@uwo.ca>
2018-09-10 16:14:38 -0400

    when fstUnsafe is called on android, return regular fst wtih default settings, since unsafe isn't supported on android

--- src/main/java/net/sciencestudio/scratch/encoders/serializers/Serializers.java
+++ src/main/java/net/sciencestudio/scratch/encoders/serializers/Serializers.java
@@ -15,11 +15,19 @@ public class Serializers {
 	}
 	
 	public static <T> ScratchEncoder<T> fstUnsafe(Class<? extends T> clazz, Class<?>... classes) {
-		return new FSTUnsafeSerializingEncoder<>(clazz, classes);
+		if (isAndroid()) {
+			return fst(clazz, classes);
+		} else {
+			return new FSTUnsafeSerializingEncoder<>(clazz, classes);
+		}
 	}
 	
 	public static <T> ScratchEncoder<T> fst(Class<? extends T> clazz, Class<?>... classes) {
 		return new FSTDefaultSerializingEncoder<>(clazz, classes);
+	}
+	
+	private static boolean isAndroid() {
+		return System.getProperty("java.vendor").equals("The Android Project");
 	}
 	
 }
