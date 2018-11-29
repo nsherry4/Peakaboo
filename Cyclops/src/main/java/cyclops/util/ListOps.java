a822b8be90af32e545db9b2c463d33faf7f41f41
Nathaniel Sherry <nsherry4@uwo.ca>
2018-11-29 16:27:31 -0500

    Added ListOps lazy permutation generator

--- Cyclops/src/main/java/cyclops/util/ListOps.java
+++ Cyclops/src/main/java/cyclops/util/ListOps.java
@@ -102,6 +102,81 @@ public class ListOps {
 		return target;
 	}
 	
+	//////////////////////////////////////////////////////////
+	// PERMUTATIONS
+	//////////////////////////////////////////////////////////
+	//thanks https://codeforces.com/blog/entry/3980
+	public static <T extends Comparable<T>> Iterable<List<T>> permutations(List<T> items) {
+		
+		
+		
+		return new Iterable<List<T>>() {
 
+			@Override
+			public Iterator<List<T>> iterator() {
+				
+				return new Iterator<List<T>>() {
+
+					List<T> copy = new ArrayList<>(items);
+					List<T> next = new ArrayList<>(copy);
+					
+					@Override
+					public boolean hasNext() {
+						return next != null;
+					}
+
+					@Override
+					public List<T> next() {
+						List<T> current = next;
+						List<T> newNext = nextPermutation(copy);
+						if (newNext != null) {
+							next = new ArrayList<>(newNext);
+						} else {
+							next = null;
+						}
+						return current;
+					}
+					
+					private List<T> nextPermutation( final List<T> c ) {
+						// 1. finds the largest k, that c[k] < c[k+1]
+						int first = getFirst( c );
+						if ( first == -1 ) return null; // no greater permutation
+						// 2. find last index toSwap, that c[k] < c[toSwap]
+						int toSwap = c.size() - 1;
+						while ( c.get(first).compareTo( c.get(toSwap) ) >= 0 )
+							--toSwap;
+						// 3. swap elements with indexes first and last
+						swap( c, first++, toSwap );
+						// 4. reverse sequence from k+1 to n (inclusive) 
+						toSwap = c.size() - 1;
+						while ( first < toSwap )
+							swap( c, first++, toSwap-- );
+						return c;
+					}
+					
+					// finds the largest k, that c[k] < c[k+1]
+					// if no such k exists (there is not greater permutation), return -1
+					private int getFirst( final List<T> list ) {
+						for ( int i = list.size() - 2; i >= 0; --i ) {
+							if ( list.get(i).compareTo( list.get(i+1)) < 0 ) {
+								return i;
+							}
+						}
+						return -1;
+					}
+					
+					// swaps two elements (with indexes i and j) in array 
+					private void swap( final List<T> c, final int i, final int j ) {
+						final T tmp = c.get(i);
+						c.set(i, c.get(j));
+						c.set(j,  tmp);
+					}
+				};
+			}};
+		
+		
+		
+	}
+	
 	
 }
