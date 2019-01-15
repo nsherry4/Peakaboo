3f22b11c8b9fc07c47d1a25fcdd80d8f4b91ee6e
Nathaniel Sherry <nsherry4@uwo.ca>
2019-01-15 11:41:42 -0500

    Windows splash screen 1px border because it doesn't add shadow to undecorated windows?

--- src/main/java/swidget/dialogues/SplashScreen.java
+++ src/main/java/swidget/dialogues/SplashScreen.java
@@ -1,11 +1,13 @@
 package swidget.dialogues;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.border.MatteBorder;
 
 import swidget.widgets.LiveFrame;
 
@@ -22,9 +24,23 @@ public class SplashScreen extends LiveFrame {
 		getContentPane().add(l, BorderLayout.CENTER);
 		l.setVisible(true);
 		
+		if (isWindows()) {
+			//windows doesn't seem to put drop shadows under undecorated windows?
+			l.setBorder(new MatteBorder(1, 1, 1, 1, new Color(0x404040)));
+		}
+		
 		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
+	}
+	
+	private static boolean isWindows()
+	{
+
+		String os = System.getProperty("os.name").toLowerCase();
+		// windows
+		return (os.indexOf("win") >= 0);
+
 	}
 
 }
