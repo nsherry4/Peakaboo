7952f9cd0322a8844028d88a59fab237f8a571bb
Nathaniel Sherry <nsherry4@uwo.ca>
2019-01-22 11:06:47 -0500

    Removed deprecation annotation fields only available in java 9+

--- LibPeakaboo/src/main/java/peakaboo/mapping/results/MapResultSet.java
+++ LibPeakaboo/src/main/java/peakaboo/mapping/results/MapResultSet.java
@@ -160,7 +160,7 @@ public class MapResultSet implements Cloneable
 	 * 
 	 * @return a list of double values representing the composited map
 	 */
-	@Deprecated(forRemoval=true, since="5.3")
+	@Deprecated //removal in 5.3
 	public Spectrum sumAllTransitionSeriesMaps(CalibrationProfile profile)
 	{
 		return sumGivenTransitionSeriesMaps(getAllTransitionSeries(), profile);
@@ -173,7 +173,7 @@ public class MapResultSet implements Cloneable
 	 * 
 	 * @return a list of double values representing the composited map
 	 */
-	@Deprecated(forRemoval=true, since="5.3")
+	@Deprecated //removal in 5.3
 	public Spectrum sumGivenTransitionSeriesMaps(Collection<ITransitionSeries> list, CalibrationProfile profile)
 	{
 		
