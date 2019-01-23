da192a875da39605ab18c429124d57a0be1b9bb0
Nathaniel Sherry <nsherry4@uwo.ca>
2019-01-23 15:01:46 -0500

    YamlSerializer correcting for old 'peakaboo.' package names in various settings files. Updated documentation on plugins to new package names

--- LibPeakaboo/src/main/java/org/peakaboo/common/YamlSerializer.java
+++ LibPeakaboo/src/main/java/org/peakaboo/common/YamlSerializer.java
@@ -11,7 +11,20 @@ public class YamlSerializer {
 	 * Decodes a serialized data object from yaml
 	 */
 	public static <T> T deserialize(String yaml) {
+
+		
+		/*
+		 * Between 5.2 and 5.3 package names were changed to use the peakaboo.org domain
+		 * name. This results in problems when loading older serialized files which
+		 * specify classnames with the old peakaboo top level package name. We do a
+		 * simple string find and replace to try and keep things working between
+		 * versions
+		 */
+		//TODO: remove in Peakaboo 6
+		yaml = yaml.replace("!!peakaboo.", "!!org.peakaboo.");
+		yaml = yaml.replace(": peakaboo.", ": org.peakaboo.");
 				
+		
 		//be forgiving of fields which are unfamiliar, this may 
 		//be a peakaboo session file from the future
 		Representer representer = new Representer();
