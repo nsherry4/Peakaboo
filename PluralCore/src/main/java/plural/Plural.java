a58b641aaaa68bd2478c35a2c0eb13ccd2cc6cd0
Nathaniel Sherry <nsherry4@uwo.ca>
2019-01-25 14:21:51 -0500

    Added Plural.build convenience method for building simple single-executor ExecutorSets

--- PluralCore/src/main/java/plural/Plural.java
+++ PluralCore/src/main/java/plural/Plural.java
@@ -1,6 +1,13 @@
 package plural;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.logging.Logger;
+
+import plural.executor.AbstractExecutor;
+import plural.executor.DummyExecutor;
+import plural.executor.ExecutorSet;
 
 public class Plural {
 
@@ -8,6 +15,33 @@ public class Plural {
 		StackTraceElement[] stElements = Thread.currentThread().getStackTrace();
 		Logger logger = Logger.getLogger( stElements[0].getClassName() );
 		return logger;
+	}
+	
+	/**
+	 * Builds a simple ExecutorSet with a single DummyExecutor to performn work provided by a function.
+	 */
+	public static <T> ExecutorSet<T> build(String title, String taskname, BiFunction<ExecutorSet<T>, AbstractExecutor, T> function) {
+		
+		DummyExecutor task = new DummyExecutor();
+		task.setName(taskname);
+		
+		ExecutorSet<T> execset = new ExecutorSet<T>(title) {
+
+			@Override
+			protected T execute() {
+				
+				task.advanceState();
+				T result = function.apply(this, task);
+				task.advanceState();
+				return result;
+				
+			}
+		};
+		
+		execset.addExecutor(task);
+		
+		return execset;
+		
 	}
 	
 }
