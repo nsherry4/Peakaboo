0d2251e3dc757c022ba53ec3177e4dbf82d61c79
Nathaniel Sherry <nsherry4@uwo.ca>
2019-11-01 15:13:30 -0400

    Correlation map colour palette honours monochrome setting
    Closes #216

--- LibPeakaboo/src/main/java/org/peakaboo/display/map/modes/correlation/CorrelationMapMode.java
+++ LibPeakaboo/src/main/java/org/peakaboo/display/map/modes/correlation/CorrelationMapMode.java
@@ -56,7 +56,11 @@ public class CorrelationMapMode extends MapMode {
 		map.setDrawingRequest(dr);
 
 		List<AbstractPalette> paletteList = new ArrayList<AbstractPalette>();
-		paletteList.add(new ColourListPalette(Spectrums.generateSpectrum(spectrumSteps, Palette.GEORGIA.getPaletteData(), 1f, 1f), false));
+		if (settings.monochrome) {
+			paletteList.add(new ColourListPalette(Spectrums.generateSpectrum(spectrumSteps, Palette.MONOCHROME_INVERTED.getPaletteData(), 1f, 1f), false));
+		} else {
+			paletteList.add(new ColourListPalette(Spectrums.generateSpectrum(spectrumSteps, Palette.GEORGIA.getPaletteData(), 1f, 1f), false));
+		}
 		
 		List<AxisPainter> axisPainters = new ArrayList<AxisPainter>();
 		super.setupTitleAxisPainters(settings, axisPainters);
