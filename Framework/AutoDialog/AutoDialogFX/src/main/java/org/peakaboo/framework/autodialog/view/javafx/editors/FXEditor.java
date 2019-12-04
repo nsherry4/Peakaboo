8102228dcc294a5ba7403c14630247c4efdddc18
Nathaniel Sherry <nsherry4@uwo.ca>
2019-12-04 11:28:05 -0500

    Removed autodialog-javafx

--- Framework/AutoDialog/AutoDialogFX/src/main/java/org/peakaboo/framework/autodialog/view/javafx/editors/FXEditor.java
+++
@@ -1,8 +0,0 @@
-package org.peakaboo.framework.autodialog.view.javafx.editors;
-
-import org.peakaboo.framework.autodialog.view.editors.Editor;
-import org.peakaboo.framework.autodialog.view.javafx.FXView;
-
-public interface FXEditor<T> extends Editor<T>, FXView {
-	
-}
