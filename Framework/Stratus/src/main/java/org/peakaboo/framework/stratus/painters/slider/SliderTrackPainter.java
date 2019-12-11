4922e3212cdda3cf3f7ac0c2bc1f59cbe5838b39
Nathaniel Sherry <nsherry4@uwo.ca>
2019-12-11 13:55:23 -0500

    Swidget tweaks to slider painter

--- Framework/Stratus/src/main/java/org/peakaboo/framework/stratus/painters/slider/SliderTrackPainter.java
+++ Framework/Stratus/src/main/java/org/peakaboo/framework/stratus/painters/slider/SliderTrackPainter.java
@@ -25,16 +25,16 @@ public class SliderTrackPainter extends SimpleThemed implements Painter<JCompone
 	@Override
 	public void paint(Graphics2D g, JComponent object, int width, int height) {
 		
-		float radius = getTheme().borderRadius();
+		float radius = Math.max(1.5f, getTheme().borderRadius());
 		
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
 
-		Shape area = new RoundRectangle2D.Float(0, height/2-2, width-1, 4, radius, radius);
+		Shape area = new RoundRectangle2D.Float(0, height/2-1.5f, width-1, 3, radius, radius);
 		
 		//Fill
 		if (enabled) {
-			g.setPaint(new LinearGradientPaint(0, height/2-2, 0, height/2-2+4, new float[] {0, 1f}, new Color[] {getTheme().getWidgetBorder(), getTheme().getControl()}));
+			g.setPaint(new LinearGradientPaint(0, height/2-1.5f, 0, height/2+1.5f, new float[] {0, 1f}, new Color[] {getTheme().getWidgetBorder(), getTheme().getControl()}));
 			g.fill(area);
 		}
 		
