e45c82f2efef3dd5f02965df9b43c91c0ab93d21
Nathaniel Sherry <nsherry4@uwo.ca>
2020-01-15 11:22:35 -0500

    windows launch4j not working with newer versions of java anymore? falling back to 8

--- Packaging/windows/build.sh
+++ Packaging/windows/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-java --illegal-access=permit -jar ./launch4j/launch4j.jar peakaboo.xml 
+#java --illegal-access=permit -jar ./launch4j/launch4j.jar peakaboo.xml 
+/usr/java/jdk1.8.0_171-amd64/bin/java -jar ./launch4j/launch4j.jar peakaboo.xml 
 mv "./build/Peakaboo.exe" "./build/Peakaboo $1.exe"
