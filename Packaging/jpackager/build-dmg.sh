66c9393bcb9aebd2216a5461aed6847a1ef70765
Nathaniel Sherry <nsherry4@uwo.ca>
2021-02-16 10:04:11 -0500

    Packaging requires min heap size of 1g

--- Packaging/jpackager/build-dmg.sh
+++ Packaging/jpackager/build-dmg.sh
@@ -11,12 +11,12 @@ rm -rf "./output/osx/"
 	--add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.logging,java.naming,java.prefs,java.rmi,java.scripting,java.sql,jdk.jdi,jdk.unsupported \
 	--version "$1" \
 	--jvm-args "--illegal-access=permit" \
+	--java-options "Xms1g" \
 	--icon "peakaboo.icns" \
 	--mac-bundle-name "Peakaboo" \
 	--mac-bundle-identifier "peakaboo.ui.swing.Peakaboo"
 
-
-bin/osx/create-dmg/create-dmg \
+./bin/osx/create-dmg/create-dmg \
 	--volname "Peakaboo $1" \
 	--volicon "peakaboo.icns" \
 	--icon-size 128 \
