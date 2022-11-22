84fba20a8cf1700055d1c13f2353bbd1c334c076
Nathaniel Sherry <nsherry4@uwo.ca>
2022-11-22 14:29:04 -0500

    Add windows and mac build jobs

--- Packaging/build-macos.sh
+++ Packaging/build-macos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/Software/Java15/bin/jpackage @version @platform/common/args @platform/macos/args
+jpackage @version @platform/common/args @platform/macos/args
--- Packaging/build-windows.bat
+++ Packaging/build-windows.bat
@@ -1,1 +1,1 @@
-C:\Users\Nathaniel\Software\Java15\bin\jpackage.exe @version @platform/common/args @platform/windows/args
+jpackage.exe @version @platform/common/args @platform/windows/args
