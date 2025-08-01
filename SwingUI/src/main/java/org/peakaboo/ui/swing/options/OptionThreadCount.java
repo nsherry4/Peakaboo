f5be575696c7fffbf32a86eb8a4f9c25bab36725
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2025-07-31 23:08:04 -0400

    Lift OptionThreadCount into a generic spinner control with optional reset button

--- SwingUI/src/main/java/org/peakaboo/ui/swing/options/OptionThreadCount.java
+++ SwingUI/src/main/java/org/peakaboo/ui/swing/options/OptionThreadCount.java
@@ -1,80 +1,24 @@
 package org.peakaboo.ui.swing.options;
 
-import java.awt.FlowLayout;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-import javax.swing.JComponent;
-import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 
 import org.peakaboo.framework.plural.Plural;
-import org.peakaboo.framework.stratus.api.icons.IconSize;
-import org.peakaboo.framework.stratus.api.icons.StockIcon;
-import org.peakaboo.framework.stratus.components.ui.fluentcontrols.button.FluentButton;
-import org.peakaboo.framework.stratus.components.ui.fluentcontrols.button.FluentButtonConfig.BorderStyle;
-import org.peakaboo.framework.stratus.components.ui.fluentcontrols.button.FluentButtonSize;
 import org.peakaboo.framework.stratus.components.ui.options.OptionBlock;
-import org.peakaboo.framework.stratus.components.ui.options.OptionCustomComponent;
+import org.peakaboo.framework.stratus.components.ui.options.OptionSpinner;
 
-public class OptionThreadCount extends OptionCustomComponent {
-
-
+public class OptionThreadCount extends OptionSpinner<Integer> {
 	
 	public OptionThreadCount(OptionBlock block, Supplier<Integer> getter, Consumer<Integer> setter) {
-		super(block, new ThreadCountComponent(setter, getter), false);
-	}
-	
-
-	
-	private static class ThreadCountComponent extends JComponent {
+		super(block, new SpinnerNumberModel(getter.get().intValue(), 1, Math.max(Plural.cores() * 2, 24), 1));
 		
-		public JSpinner spinner;
-		public FluentButton reset;
-		
-		private Consumer<Integer> setter;
-		private Supplier<Integer> getter;
-		
-		public ThreadCountComponent(Consumer<Integer> setter, Supplier<Integer> getter) {
-			this.setter = setter;
-			this.getter = getter;
-			
-			spinner = new JSpinner(new SpinnerNumberModel((int)getter.get(), 1, (int)Math.max(Plural.cores() * 2, 24), 1));
-			spinner.setMaximumSize(spinner.getPreferredSize());
-			
-			//Listeners for the size textbox
-			spinner.addChangeListener(change -> {
-				set((int)spinner.getValue());
-			});
-			
-			reset = new FluentButton()
-				.withIcon(StockIcon.EDIT_UNDO, IconSize.BUTTON)
-				.withBordered(BorderStyle.ACTIVE)
-				.withTooltip("Reset to default")
-				.withButtonSize(FluentButtonSize.COMPACT)
-				.withAction(() -> {
-					int defaultCores = Plural.cores();
-					setter.accept(defaultCores);
-					spinner.getModel().setValue(defaultCores);
-				});
-			
-			this.setLayout(new FlowLayout());
-			this.add(reset);
-			this.add(spinner);
-			
-		}
-		
-		void set(int value) {
-			try {
-				if (value != getter.get()) {
-					setter.accept(value);
-				}
-			} catch (NumberFormatException e) {
-				
+		this.withListener(value -> {
+			if (!value.equals(getter.get())) {
+				setter.accept(value);
 			}
-		}
-		
+		}).withResetButton(Plural::cores);
 	}
-	
 	
 }
