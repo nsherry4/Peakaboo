641247ca7ebb8a7d36155c96fa7683a55302aba6
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2025-11-11 17:32:03 -0500

    Fix peak table issue with revised range implementation

--- LibPeakaboo/src/main/java/org/peakaboo/curvefit/peak/table/XrayLibPeakTable.java
+++ LibPeakaboo/src/main/java/org/peakaboo/curvefit/peak/table/XrayLibPeakTable.java
@@ -47,25 +47,25 @@ public class XrayLibPeakTable implements PeakTable {
 		
 		//K Lines
 		RangeSet kLines = new RangeSet();
-		kLines.addRange(new Range(Xraylib.KP5_LINE, Xraylib.KL1_LINE+1));
+		kLines.addRange(new Range(Xraylib.KP5_LINE, Xraylib.KL1_LINE));
 		
 		
 		//L Lines
 		RangeSet lLines = new RangeSet();
-		lLines.addRange(new Range(Xraylib.L3P4_LINE, Xraylib.L1L2_LINE+1));
+		lLines.addRange(new Range(Xraylib.L3P4_LINE, Xraylib.L1L2_LINE));
 		//Remove L1->L*
-		lLines.removeRange(new Range(Xraylib.L1L3_LINE, Xraylib.L1L2_LINE+1));
+		lLines.removeRange(new Range(Xraylib.L1L3_LINE, Xraylib.L1L2_LINE));
 		//Remove L2->L*
-		lLines.removeRange(new Range(Xraylib.L2L3_LINE, Xraylib.L2L3_LINE+1));
+		lLines.removeRange(new Range(Xraylib.L2L3_LINE, Xraylib.L2L3_LINE));
 				
 		//M Lines
 		RangeSet mLines = new RangeSet();
-		mLines.addRange(new Range(Xraylib.M5P5_LINE, Xraylib.M1M2_LINE+1));
+		mLines.addRange(new Range(Xraylib.M5P5_LINE, Xraylib.M1M2_LINE));
 		//Remove lines from one m to another
-		mLines.removeRange(new Range(Xraylib.M1M5_LINE, Xraylib.M1M2_LINE+1));
-		mLines.removeRange(new Range(Xraylib.M2M5_LINE, Xraylib.M2M3_LINE+1));
-		mLines.removeRange(new Range(Xraylib.M3M5_LINE, Xraylib.M3M4_LINE+1));
-		mLines.removeRange(new Range(Xraylib.M4M5_LINE, Xraylib.M4M5_LINE+1));	
+		mLines.removeRange(new Range(Xraylib.M1M5_LINE, Xraylib.M1M2_LINE));
+		mLines.removeRange(new Range(Xraylib.M2M5_LINE, Xraylib.M2M3_LINE));
+		mLines.removeRange(new Range(Xraylib.M3M5_LINE, Xraylib.M3M4_LINE));
+		mLines.removeRange(new Range(Xraylib.M4M5_LINE, Xraylib.M4M5_LINE));
 		
 		for (Element e : Element.values()) {
 			readElementShell(kLines, e, TransitionShell.K);
@@ -81,16 +81,17 @@ public class XrayLibPeakTable implements PeakTable {
 		float maxRel = 0f;
 		for (int line : lines) {
 			if (!hasLine(elem, line)) continue;
-			maxRel = Math.max(maxRel, lineRelativeIntensity(elem, line));	
+			maxRel = Math.max(maxRel, lineRelativeIntensity(elem, line));
 		}
 		for (int line : lines) {
+			// maxRel will be set as long as one of these lines continued in the last block
 			if (!hasLine(elem, line)) continue;
 			
 			float value = (float) Xraylib.LineEnergy(elem.atomicNumber(), line);
 			float rel = lineRelativeIntensity(elem, line) / maxRel;
 			
-			//don't bother with this if the line is <0.1% the intensity of the largest line
-			if (rel < maxRel*0.001) { continue; }
+			//don't bother with this if the line is <1% of the normalized intensity
+			if (rel < 0.01) { continue; }
 			
 			Transition t = new Transition(value, rel, elem.name() + " " + tstype.name() + " #" + line + " @" + value + " keV x " + rel*100 + "%");
 			ts.addTransition(t);
