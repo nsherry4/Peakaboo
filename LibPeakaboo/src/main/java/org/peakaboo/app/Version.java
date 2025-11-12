513b2895fe98d851a379987090ccd8417102212b
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2025-11-12 14:29:18 -0500

    Bump minor version

--- LibPeakaboo/src/main/java/org/peakaboo/app/Version.java
+++ LibPeakaboo/src/main/java/org/peakaboo/app/Version.java
@@ -15,7 +15,7 @@ import org.peakaboo.tier.Tier;
 public class Version {
 	
 	public final static int VERSION_MAJOR = 6;
-	public final static int VERSION_MINOR = 0;
+	public final static int VERSION_MINOR = 1;
 	public final static int VERSION_POINT = 0;
 	//Program name intended for internal use, filesystem, etc. For user-friendly name, see Tier.appName()
 	public final static String PROGRAM_NAME = "Peakaboo";
