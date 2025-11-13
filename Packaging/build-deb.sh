d79d8974a2106a497f21aa18fef96e07a363d694
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2025-11-13 08:14:35 -0500

    Improve support for building dev and release types

--- Packaging/build-deb.sh
+++ Packaging/build-deb.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-jpackage @version @platform/common/args @platform/linux/args --type deb
+RELEASE_TYPE=${1:-release}
+
+jpackage @version @platform/common/args @platform/common/args-${RELEASE_TYPE} @platform/linux/args @platform/linux/args-${RELEASE_TYPE} --type deb
