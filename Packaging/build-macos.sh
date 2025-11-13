e96e1ecac7a878f5758c7a808c6fc59579730b1c
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2025-11-13 08:14:35 -0500

    Improve support for building dev and release types

--- Packaging/build-macos.sh
+++ Packaging/build-macos.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-jpackage @version @platform/common/args @platform/macos/args
+RELEASE_TYPE=${1:-release}
+
+jpackage @version @platform/common/args @platform/common/args-${RELEASE_TYPE} @platform/macos/args @platform/macos/args-${RELEASE_TYPE}
--- Packaging/build-windows.bat
+++ Packaging/build-windows.bat
@@ -1,1 +1,5 @@
-jpackage.exe @version @platform/common/args @platform/windows/args
+@echo off
+set RELEASE_TYPE=%1
+if "%RELEASE_TYPE%"=="" set RELEASE_TYPE=release
+
+jpackage.exe @version @platform/common/args @platform/common/args-%RELEASE_TYPE% @platform/windows/args @platform/windows/args-%RELEASE_TYPE%
